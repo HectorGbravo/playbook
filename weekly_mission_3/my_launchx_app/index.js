d7e81790f9e1bbc224e96ce61a51dde230db677e
HectorGbravo <launchx04896@innovaccion.mx>
2022-04-20 19:00:07 -0500

    Update: first jest test

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo");
+const hector = new MissionCommander("Héctor");
+const luis = new MissionCommander("Luis");
+console.log(carlo.name)
+console.log(hector.name)
+console.log(luis.name)
