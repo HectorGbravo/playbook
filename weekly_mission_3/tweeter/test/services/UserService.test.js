7533dc0ccdc73a3adc4d2d057923bf86eddfc47d
mexmovil gbd <viajero08307@innovaccion.mx>
2022-04-25 17:33:34 -0500

    Added & tested 2nd requeriment

--- weekly_mission_3/tweeter/test/services/UserService.test.js
+++ weekly_mission_3/tweeter/test/services/UserService.test.js
@@ -8,4 +8,13 @@ describe('Test for user service', () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     })
+    test('2. Get all user data in a list',() => {
+        const user = UserService.create(1,"HectorGbravo", "Héctor Bravo")
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("HectorGbravo")
+        expect(userInfoInList[2]).toBe("Héctor Bravo")
+        expect(userInfoInList[3]).toBe("Sin Bio")
+    })
+
 })
