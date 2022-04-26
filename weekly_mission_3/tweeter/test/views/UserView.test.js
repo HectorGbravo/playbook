4743ddecd767ac19a2446c519acb6bed49850271
mexmovil gbd <viajero08307@innovaccion.mx>
2022-04-26 15:07:55 -0500

    5. Views: Added & tested the 3rd request

--- weekly_mission_3/tweeter/test/views/UserView.test.js
+++ weekly_mission_3/tweeter/test/views/UserView.test.js
@@ -8,8 +8,13 @@ describe('Tests for Userview', () => {
         expect(result.error).toMatch(/the payload does not exist/)
 
     })
-    test ("2. Return an error object when try to create a new user with a payload with invalid propieties", () => {
+    test ("2. Return an error object when try to create a new user with a payload with invalid properties", () => {
         const payload = {username: null, name:12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/Error, the payload properties must be a valid value/)
+    })
+    test ("3. Return an error object when try to create a new user with a payload with missing properties",() => {
+        const payload = {username: "Username"}
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/Error, the payload properties must be a valid value/)
     })
