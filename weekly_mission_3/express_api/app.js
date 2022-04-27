6f653f045eae775bcb41b8ca18998f4a8ca68766
mexmovil gbd <viajero08307@innovaccion.mx>
2022-04-27 00:10:09 -0500

    Firts API. POST Create an endpoint that is responsible for creating an explorer.

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -12,6 +12,8 @@ app.listen(port, () => {
 })
 
 // Métodos HTTP
+
+// GET Crea un endpoint que regrese una lista de explorers
 app.get('/v1/explorers', (req, res) => {
     console.log(`API Explorers GET ALL Requests ${new Date()}`)
     const explorer1 = {id: 1, name: "Hector"}
@@ -22,6 +24,7 @@ app.get('/v1/explorers', (req, res) => {
     res.status(200).json(explorers)
 })
 
+// GET Crea un endpoint que te regrese un explorer mediando un ID
 app.get('/v1/explorers/:id', (req, res) => {
     console.log(`API explorers GET request ${new Date()}`)
     console.log(`Getting explorer with id ${req.params.id}`)
@@ -29,4 +32,9 @@ app.get('/v1/explorers/:id', (req, res) => {
     res.status(200).json(explorer)
 })
 
-
+// POST Crea un endpoint que se encargue de crear un explorer
+app.post('/v1/explorers', (req, res) => {
+    console.log(`API explorers POST request ${new Date()}`)
+    const requestBody = req.body // Parámetros de un cliente
+    res.status(201).json({message: "Created"})
+})
